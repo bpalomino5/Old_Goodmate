dc6dcce6431b9c82c5568b5ec4328341c1fd2f9e
Brandon Palomino <12bpalomino@gmail.com>
2017-07-10 20:21:27 -0700

    created view_holder class

--- app/src/main/java/com/bpalomino/goodmate/View_Holder.java
+++ app/src/main/java/com/bpalomino/goodmate/View_Holder.java
@@ -1,8 +1,25 @@
 package com.bpalomino.goodmate;
 
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by bpalomino on 7/10/17.
  */
 
-class View_Holder {
+public class View_Holder extends RecyclerView.ViewHolder{
+    CardView cardView;
+    TextView title;
+    TextView description;
+    ImageView imageView;
+
+    public View_Holder(View itemView) {
+        super(itemView);
+        cardView = itemView.findViewById(R.id.cardView);
+        title = itemView.findViewById(R.id.title);
+        imageView = itemView.findViewById(R.id.imageView);
+    }
 }
