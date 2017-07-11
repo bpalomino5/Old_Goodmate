c7b260a144fb4ad7f7a6e94c1112e842007668e9
Brandon Palomino <12bpalomino@gmail.com>
2017-07-10 20:05:06 -0700

    created shareitem class for recyclerview

---
+++ app/src/main/java/com/bpalomino/goodmate/ShareItem.java
@@ -0,0 +1,17 @@
+package com.bpalomino.goodmate;
+
+/**
+ * Created by bpalomino on 7/10/17.
+ */
+
+public class ShareItem {
+    public String title;
+    public String description;
+    public int imageId;
+
+    public ShareItem(String title, String description, int imageId) {
+        this.title = title;
+        this.description = description;
+        this.imageId = imageId;
+    }
+}
