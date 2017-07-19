941360b640768a9b227673ef1edc3378bb52a7b1
Brandon Palomino <12bpalomino@gmail.com>
2017-07-19 07:00:35 -0700

    switched to expandable list view for creating rent sheet

--- app/src/main/java/com/bpalomino/goodmate/AddRentSheetActivity.java
+++ app/src/main/java/com/bpalomino/goodmate/AddRentSheetActivity.java
@@ -3,16 +3,25 @@ package com.bpalomino.goodmate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/*  This Activity serves to do the following:
+    Use listviews to create a rent sheet
+*/
+
 public class AddRentSheetActivity extends AppCompatActivity {
-    @BindView(R.id.addRent_list_view) ListView rentList;
-    private ArrayList<RentGroup> items;
+    @BindView(R.id.addRent_list_view)
+    ExpandableListView rentList;
+    private ArrayList<RentGroup> groupItems;
+    private ArrayList<String> dataHeaders;
+    private HashMap<String,ArrayList<RentGroup>> dataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +30,31 @@ public class AddRentSheetActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         //data source list
-        items = new ArrayList<>();
+//        groupItems = new ArrayList<>();
 
         //need one starter blank item
-        items.add(new RentGroup(R.drawable.ic_add_circle_outline_24dp,0));
+//        groupItems.add(new RentGroup(R.drawable.ic_add_circle_outline_24dp,0));
 
-        // custom adapter for listview
-        RentSheetAdapter adapter = new RentSheetAdapter(this, items);
+        //populate list with items
+        populateList();
+
+        // custom adapter for expandable listview
+        RentSheetAdapter adapter = new RentSheetAdapter(this, dataHeaders,dataChild);
+
         rentList.setAdapter(adapter);
+    }
+
+    private void populateList() {
+        dataHeaders = new ArrayList<>();
+        dataChild = new HashMap<>();
+
+        //adding header data
+        dataHeaders.add("Group Name");
+
+        //adding child data
+        ArrayList<RentGroup> items = new ArrayList<>();
+        items.add(new RentGroup(R.drawable.ic_add_circle_outline_24dp,0));
+        dataChild.put(dataHeaders.get(0),items);
+
     }
 }
--- app/src/main/java/com/bpalomino/goodmate/RentSheetAdapter.java
+++ app/src/main/java/com/bpalomino/goodmate/RentSheetAdapter.java
@@ -1,16 +1,20 @@
 package com.bpalomino.goodmate;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,87 +23,137 @@ import butterknife.ButterKnife;
  * Created by bpalomino on 7/17/17.
  */
 
-public class RentSheetAdapter extends BaseAdapter {
+public class RentSheetAdapter extends BaseExpandableListAdapter {
     private Context context;
-    private LayoutInflater inflater;
-    private ArrayList<RentGroup> dataSource;
+    private ArrayList<String> dataHeaders;
+    private HashMap<String, ArrayList<RentGroup>> dataChild;
 
-    @BindView(R.id.groupView) EditText editText;
-    @BindView(R.id.groupAddButton) ImageButton addGroupButton;
+//    private ArrayList<RentGroup> dataSource;
 
-    public RentSheetAdapter(Context context, ArrayList<RentGroup> dataSource) {
+    public class ChildViewHolder {
+        @BindView(R.id.groupView)
+        EditText editText;
+        @BindView(R.id.groupAddButton)
+        ImageButton addGroupButton;
+    }
+    @BindView(R.id.listHeaderView)
+    TextView listHeaderView;
+
+    private ChildViewHolder childViewHolder;
+    public RentSheetAdapter(Context context, ArrayList<String> dataHeaders, HashMap<String,ArrayList<RentGroup>> dataChild) {
         this.context = context;
-        this.dataSource = dataSource;
-        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-
-
-    @Override
-    public int getCount() {
-        return dataSource.size();
+        this.dataHeaders = dataHeaders;
+        this.dataChild = dataChild;
+        childViewHolder = new ChildViewHolder();
     }
 
     @Override
-    public Object getItem(int i) {
-        return dataSource.get(i);
+    public int getGroupCount() {
+        return dataHeaders.size();
     }
 
     @Override
-    public long getItemId(int i) {
+    public int getChildrenCount(int i) {
+        return dataChild.get(dataHeaders.get(i)).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return dataHeaders.get(i);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return dataChild.get(dataHeaders.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int i) {
         return i;
     }
 
     @Override
-    public View getView(final int i, View view, ViewGroup viewGroup) {
-        //all the implementation happens here
-        View rowView = inflater.inflate(R.layout.list_rent_add_item, viewGroup, false);
-        ButterKnife.bind(this, rowView);
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        String title = (String) getGroup(i);
+        if(view==null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_group,null);
+        }
+        ButterKnife.bind(this,view);
+
+
+//        listHeaderView.setTypeface(null, Typeface.BOLD);
+
+        return view;
+    }
+
+    @Override
+    public View getChildView(final int i, final int i1, boolean b, View view, ViewGroup viewGroup) {
+        if(view==null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_rent_add_item, viewGroup, false);
+        }
+        ButterKnife.bind(childViewHolder, view);
 
         //get item from datasource
-        RentGroup item = (RentGroup) getItem(i);
+            RentGroup item = (RentGroup) getChild(i,i1);
 
-        //set edittext to its group name
-        editText.setText(item.group);
 
-        //listener to see when text is inputted by user
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            //set edittext to its group name
+            childViewHolder.editText.setText(item.group);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                //update data source on new group name
-                dataSource.get(i).group = editable.toString().trim();
-            }
-        });
-
-        //set image from data source
-        addGroupButton.setImageResource(item.imageRes);
-        addGroupButton.setTag(item.tag);
-
-        //listener to see if button selected
-        addGroupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //identify which button it is - add/remove
-                ImageButton button = (ImageButton) view;
-                if((int)button.getTag() == 0)
-                    dataSource.add(new RentGroup(R.drawable.ic_remove_circle_outline_24dp,1));
-                else{
-                    dataSource.remove(i);
+            //listener to see when text is inputted by user
+            childViewHolder.editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 }
-                //update user's screen with changes
-                notifyDataSetChanged();
-            }
-        });
 
-        return rowView;
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    //update data source on new group name
+                    dataChild.get(dataHeaders.get(i)).get(i1).group = editable.toString().trim();
+                }
+            });
+
+            //set image from data source
+            childViewHolder.addGroupButton.setImageResource(item.imageRes);
+            childViewHolder.addGroupButton.setTag(item.tag);
+
+            //listener to see if button selected
+            childViewHolder.addGroupButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //identify which button it is - add/remove
+                    ImageButton button = (ImageButton) view;
+                    if ((int) button.getTag() == 0)
+                        dataChild.get(dataHeaders.get(i)).add(new RentGroup(R.drawable.ic_remove_circle_outline_24dp, 1));
+                    else
+                        dataChild.get(dataHeaders.get(i)).remove(i1);
+                    //update user's screen with changes
+                    notifyDataSetChanged();
+                }
+            });
+
+
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
     }
 }
